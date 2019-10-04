64d634ab55c5b78e4a751dd3809eded6ecbed529
Mario Alberto Barragán Espinosa <>
2019-10-03 20:19:01 -0500

    Creating a simple browser with WKWebView

---
+++ Project4/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Mario Alberto Barragán Espinosa on 10/3/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+
+}
+
